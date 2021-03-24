5ec8da4405b1a9f6ba98c8ccad08f49dd4219360
Ricardo Fantinelli <45544590+ricarfantinelli@users.noreply.github.com>
2021-03-24 21:04:35 +0100

    Change
    Not necessary to expose fake passwords in config.

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'udacity-ricfanta.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'udacity-ricfanta-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'p@ssword1234'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'xxxxxx'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 #
